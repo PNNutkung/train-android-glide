5c01dbea54a2353663436da538a2b4f0835796d6
PNNutkung <pipatpol.t@ku.th>
2016-06-14 17:12:52 +0700

    Show picture

---
+++ app/src/main/java/com/example/nut/sampleglider/GlideConfig/GlideConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.nut.sampleglider.GlideConfig;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * Created by nut on 14/6/2559.
+ */
+public class GlideConfiguration implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+
+    }
+}
--- app/src/main/java/com/example/nut/sampleglider/MainActivity.java
+++ app/src/main/java/com/example/nut/sampleglider/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.nut.sampleglider;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String SAMPLE_IMAGE_URL = "http://nuuneoi.com/uploads/source/playstore/cover.jpg";
+
+    @BindView(R.id.main_image_view)
+    ImageView mainImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Glide.with(this)
+                .load(SAMPLE_IMAGE_URL)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .fitCenter()
+                .crossFade()
+                .into(mainImageView);
     }
 }
